cf23716f710c72b67eb5697bdcd1fad67ee44910
Matheus351 <matheusniklaus@gmail.com>
2023-04-04 19:58:04 -0300

    configuracao inicial da api

---
+++ src/api/api.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const BASE_URl = "https://api.themoviedb.org/3";
+const API_TOKEN = import.meta.env.APY_KEY;
+
+const headers = {
+  Authorization: "Bearer " + API_TOKEN,
+};
+
+export const fetchData = async (url: string, params: string) => {
+  try {
+    const { data } = await axios.get(BASE_URl + url, {
+      headers,
+      params,
+    });
+
+    return data;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
+
+export default {};
