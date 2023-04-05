67f3a9753752d31cba665f12dddf632463e9e91e
Matheus351 <matheusniklaus@gmail.com>
2023-04-05 10:47:45 -0300

    adicionando Carousel de filmes Trending

--- src/components/genres/Genres.tsx
+++ src/components/genres/Genres.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
 
-const Genres = () => {
-  return (
-    <div>Genres</div>
-  )
-}
+import "./style.scss";
 
-export default Genres
+const Genres = ({ data }:any) => {
+    const { genres } = useSelector((state:any) => state.home);
+
+    return (
+        <div className="genres">
+            {data?.map((g:any) => {
+                if (!genres[g]?.name) return;
+                return (
+                    <div key={g} className="genre">
+                        {genres[g]?.name}
+                    </div>
+                );
+            })}
+        </div>
+    );
+};
+
+export default Genres;
